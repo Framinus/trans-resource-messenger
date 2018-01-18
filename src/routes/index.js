36f7d2a4f858514e37310f7284948c3bdf2e2ccf
James McCormack <thejamesmccormack@gmail.com>
2018-01-18 12:59:23 -0800

    converted all user replies to uppercase and put in inspiration array randomizer

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,28 +3,30 @@ const MessagingResponse = require('twilio').twiml.MessagingResponse;
 const replies = require('../data/messages');
 
 router.post('/sms', (req, res) => {
-  const messageData = req.body.Body;
-  console.log('messageData', messageData);
+  // pulls data from sent message
+  const messageData = req.body.Body.toUpperCase();
 
   const smsCount = req.session.counter || 0;
 
+  // if this is the first text being sent to the number, the user gets a welcome message.
   let message = replies.welcome;
 
+  // after that, they receive a response based on the word that they text. Reply text is kept in a JSON file.
   if (smsCount > 0) {
-    if (messageData === 'TALK') {
+    if (messageData === "TALK") {
       message = replies.talk;
     } else if (messageData === 'LEGAL') {
       message = replies.legal;
-    } else if (messageData === 'GLITTER') {
-      message = replies.inspiration;
+    } else if (messageData === 'LOVE') {
+      const x = Math.floor(Math.random() * (replies.inspiration.length - 1));
+      message = replies.inspiration[x];
     } else {
       message = replies.repeat;
     }
   }
-  console.log('message', message);
 
   req.session.counter = smsCount + 1;
-
+  // instantiating a new message response and setting it to the custom message created above.
   const twiml = new MessagingResponse();
   twiml.message(message);
 
